2ab887f256b381407cb357c03dc48a1a2bc88b2f
TurniXXD <legokubik@seznam.cz>
2021-02-06 02:30:47 +0100

    kinda_works

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const graphql = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
 const {
   GraphQLSchema,
@@ -6,133 +7,110 @@ const {
   GraphQLString,
   GraphQLList,
   GraphQLInt,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLBoolean
 } = require('graphql')
 const app = express()
 
-//const products = require('./products.json')
-//const users = require('./users.json')
+const products = require('./products.json')
+const users = require('./users.json')
 
-const authors = [
-	{ id: 1, name: 'J. K. Rowling' },
-	{ id: 2, name: 'J. R. R. Tolkien' },
-	{ id: 3, name: 'Brent Weeks' }
-]
-
-const books = [
-	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
-	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
-	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
-	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
-	{ id: 5, name: 'The Two Towers', authorId: 2 },
-	{ id: 6, name: 'The Return of the King', authorId: 2 },
-	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
-]
-
-const BookType = new GraphQLObjectType({
-  name: 'Book',
-  description: 'This represents a book written by an author',
-  fields: () => ({
+const productType = new GraphQLObjectType({
+  name: 'Product',
+  fields: {
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
-    authorId: { type: GraphQLNonNull(GraphQLInt) },
-    author: {
-      type: AuthorType,
-      resolve: (book) => {
-        return authors.find(author => author.id === book.authorId)
-      }
-    }
-  })
+    price: { type: GraphQLNonNull(GraphQLString) },
+    color: { type: GraphQLNonNull(GraphQLString) },
+    size: { type: GraphQLNonNull(GraphQLString) },
+    instock: { type: GraphQLNonNull(GraphQLString) }
+  }
 })
 
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  description: 'This represents a author of a book',
-  fields: () => ({
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve: (author) => {
-        return books.filter(book => book.authorId === author.id)
-      }
-    }
-  })
+    email: { type: GraphQLNonNull(GraphQLString) }
+  }
 })
 
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
-    book: {
-      type: BookType,
-      description: 'A Single Book',
+    product: {
+      type: productType,
+      description: 'A single product',
       args: {
-        id: { type: GraphQLInt }
+        id: { type: GraphQLInt },
+/*         name: { type: GraphQLString },
+        price: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        size: { type: GraphQLInt },
+        instock: { type: GraphQLString }, */
       },
-      resolve: (parent, args) => books.find(book => book.id === args.id)
-    },
-    books: {
-      type: new GraphQLList(BookType),
-      description: 'List of All Books',
-      resolve: () => books
-    },
-    authors: {
-      type: new GraphQLList(AuthorType),
-      description: 'List of All Authors',
-      resolve: () => authors
-    },
-    author: {
-      type: AuthorType,
-      description: 'A Single Author',
-      args: {
-        id: { type: GraphQLInt }
-      },
-      resolve: (parent, args) => authors.find(author => author.id === args.id)
-    }
-  })
-})
-
-const RootMutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Root Mutation',
-  fields: () => ({
-    addBook: {
-      type: BookType,
-      description: 'Add a book',
-      args: {
-        name: { type: GraphQLNonNull(GraphQLString) },
-        authorId: { type: GraphQLNonNull(GraphQLInt) }
-      },
-      resolve: (parent, args) => {
-        const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
-        books.push(book)
-        return book
+      resolve: (_, args) => {
+        return products[args.id/* , args.name, args.price, args.color, args.size, args.instock */];
       }
     },
-    addAuthor: {
-      type: AuthorType,
-      description: 'Add an author',
+    products: {
+      type: new GraphQLList(productType),
+      description: 'List of all products',
       args: {
-        name: { type: GraphQLNonNull(GraphQLString) }
+        id: { type: GraphQLInt },
+/*         name: { type: GraphQLString },
+        price: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        size: { type: GraphQLInt },
+        instock: { type: GraphQLString }, */
       },
-      resolve: (parent, args) => {
-        const author = { id: authors.length + 1, name: args.name }
-        authors.push(author)
-        return author
+      resolve: () => (_, args) => {
+        return products;
+      }
+    },
+    user: {
+      type: userType,
+      description: 'A single user',
+      args: {
+        id: { type: GraphQLInt },
+/*         name: { type: GraphQLString },
+        email: { type: GraphQLString }, */
+
+      },
+      resolve: (_, args) => {
+        return users[args.id/* , args.name, args.email */];
+      }
+    },
+    users: {
+      type: new GraphQLList(userType),
+      description: 'List of all users',
+      args: {
+        id: { type: GraphQLInt },
+/*         name: { type: GraphQLString },
+        email: { type: GraphQLString }, */
+      },
+      resolve: () => (_, args) => {
+        return users;
       }
     }
   })
 })
 
 const schema = new GraphQLSchema({
-  query: RootQueryType,
-  mutation: RootMutationType
+  query: RootQueryType
 })
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true
-}))
-app.listen(5000, () => console.log('Server Running'))
+//graphiql: true => UI
+express()
+  .use('/graphql', graphqlHTTP({ 
+    schema: schema, 
+    pretty: true, 
+    graphiql: true, 
+    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json',},
+    body: JSON.stringify({
+      products, 
+      users
+    })
+  }))
+  .listen(3000), () => console.log('Server is up on port 3000');
